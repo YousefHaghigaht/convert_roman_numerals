1014467e220596ed54f0b6a90b636ad1f52d349f
yousef <yousefmpym1@gmail.com>
2024-10-14 09:05:31 +0330

    Add comments to the code

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 
+# Function to convert Roman numeral to integer
 def roman_to_integer(roman):
 
+    # Dictionary to hold Roman numeral values
     roman_values = {
         'I': 1,
         'V': 5,
@@ -31,17 +33,21 @@ def roman_to_integer(roman):
 app = FastAPI()
 
 
+# Define a Pydantic model for input validation
 class RomanInput(BaseModel):
     roman_numeral: str
 
 
+# API endpoint to convert Roman numeral to intege
 @app.post('/convert/')
 async def convert_roman_to_integer(input:RomanInput):
     roman_numeral = input.roman_numeral
 
+    # Validate the input Roman numeral
     if not all(char in 'IVXLCDM' for char in roman_numeral):
         raise HTTPException(status_code=400,detail='Invalid Roman numeral')
 
+    # Convert the Roman numeral to an integer
     integer_value = roman_to_integer(roman_numeral)
 
     return {'integer_value': integer_value}
